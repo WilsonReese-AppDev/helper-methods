fa40d791781b3af33fb6f026bb7cff0ab016348a
WilsonReese <reese@chicagobooth.edu>
2021-04-13 15:33:06 +0000

    named routes to replace in view templates

--- config/routes.rb
+++ config/routes.rb
@@ -6,19 +6,19 @@ Rails.application.routes.draw do
   # Routes for the Movie resource:
 
   # CREATE
-  post "/movies" => "movies#create"
-  get "/movies/new" => "movies#new"
+  post "/movies" => "movies#create", as: :movies # movies_url and movies_path
+  get "/movies/new" => "movies#new", as: :new_movie #new_movie_url and new_movie_path
           
   # READ
-  get "/movies" => "movies#index"
-  get "/movies/:id" => "movies#show"
+  get "/movies" => "movies#index" #already named above
+  get "/movies/:id" => "movies#show", as: :movie # movie_url() and movie_path(), expects argument to populate id segment
   
   # UPDATE
-  patch "/movies/:id" => "movies#update"
-  get "/movies/:id/edit" => "movies#edit"
+  patch "/movies/:id" => "movies#update" # already named
+  get "/movies/:id/edit" => "movies#edit", as: :edit_movie # edit_movie_url() and edit_movie_path()
   
   # DELETE
-  delete "/movies/:id" => "movies#destroy"
+  delete "/movies/:id" => "movies#destroy" # will be movie_path
 
   #------------------------------
 end
