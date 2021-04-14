3624b3389a354a0ba443adcd82dff7a5ccd34ee1
WilsonReese <reese@chicagobooth.edu>
2021-04-14 02:52:29 +0000

    used resources :movies to make routes

--- config/routes.rb
+++ config/routes.rb
@@ -5,20 +5,22 @@ Rails.application.routes.draw do
   
   # Routes for the Movie resource:
 
-  # CREATE
-  post "/movies" => "movies#create", as: :movies # movies_url and movies_path
-  get "/movies/new" => "movies#new", as: :new_movie #new_movie_url and new_movie_path
+  resources :movies
+
+  # # CREATE
+  # post "/movies" => "movies#create", as: :movies # movies_url and movies_path
+  # get "/movies/new" => "movies#new", as: :new_movie #new_movie_url and new_movie_path
           
-  # READ
-  get "/movies" => "movies#index" #already named above
-  get "/movies/:id" => "movies#show", as: :movie # movie_url() and movie_path(), expects argument to populate id segment
+  # # READ
+  # get "/movies" => "movies#index" #already named above
+  # get "/movies/:id" => "movies#show", as: :movie # movie_url() and movie_path(), expects argument to populate id segment
   
-  # UPDATE
-  patch "/movies/:id" => "movies#update" # already named
-  get "/movies/:id/edit" => "movies#edit", as: :edit_movie # edit_movie_url() and edit_movie_path()
+  # # UPDATE
+  # patch "/movies/:id" => "movies#update" # already named
+  # get "/movies/:id/edit" => "movies#edit", as: :edit_movie # edit_movie_url() and edit_movie_path()
   
-  # DELETE
-  delete "/movies/:id" => "movies#destroy" # will be movie_path
+  # # DELETE
+  # delete "/movies/:id" => "movies#destroy" # will be movie_path
 
   #------------------------------
 end
