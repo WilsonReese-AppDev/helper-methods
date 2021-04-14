722aea74ad679d58299bda3a82b4d2f4530439b5
WilsonReese <reese@chicagobooth.edu>
2021-04-14 15:51:22 +0000

    made edit, show, update and delete actions and their view templates

--- app/controllers/directors_controller.rb
+++ app/controllers/directors_controller.rb
@@ -15,6 +15,10 @@ class DirectorsController < ApplicationController
     end
   end
 
+  def show
+    @director = Director.find(params.fetch(:id))
+  end
+
   def create
     director_attributes = params.require(:director).permit(:name, :dob)
     
@@ -26,6 +30,31 @@ class DirectorsController < ApplicationController
     else
       render "new"
     end
+  end
+
+  def edit
+    @director = Director.find(params.fetch(:id))
+  end
+
+  def update
+    @director = Director.find(params.fetch(:id)) 
+    director_attributes = params.require(:director).permit(:name, :dob) 
+    @director.update(director_attributes)
+
+    if @director.valid?
+      @director.save
+      redirect_to director_url(@director), notice: "Director updated successfully."
+    else
+      redirect_to director_url(@director), alert: "Director failed to update successfully."
+    end
+  end
+
+  def destroy
+    @director = Director.find(params.fetch(:id))
+
+    @director.destroy
+
+    redirect_to directors_url, notice: "Director deleted successfully."
   end
 
 end
